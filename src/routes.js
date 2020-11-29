84486242ad2d1b746d087f7118706063bc5add82
Guilherme Humberto <gbreu@outlook.com>
2020-11-29 13:13:04 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components/native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';

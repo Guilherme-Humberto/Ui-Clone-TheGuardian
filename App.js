1445c935be08b1a55e6333c809c90cf38736a88a
Guilherme Humberto <gbreu@outlook.com>
2020-11-28 21:54:11 -0300

    Arquivo principal

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
+import "react-native-gesture-handler"
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo'
+
+import { 
+  PlayfairDisplay_400Regular, 
+  PlayfairDisplay_500Medium, 
+  PlayfairDisplay_700Bold,
+  PlayfairDisplay_900Black,
+  useFonts 
+} from '@expo-google-fonts/playfair-display'
+
+import {
+  Roboto_500Medium,
+  Roboto_400Regular
+} from '@expo-google-fonts/roboto'
+
+import { 
+  Lusitana_700Bold,
+} from '@expo-google-fonts/lusitana'
+
+import { Routes } from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_500Medium,
+    PlayfairDisplay_700Bold,
+    PlayfairDisplay_900Black,
+    Roboto_500Medium,
+    Lusitana_700Bold,
+    Roboto_400Regular
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar  style="light"/>
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
